88403949aad829f03c35ef47d5ac3d7dc766f121
abdel-boudaoud <abdelkaderfarm@gmail.com>
2025-06-30 18:36:33 +0100

    scrape the emails from a list of websites]

--- index.js
+++ index.js
@@ -2,23 +2,34 @@ const axios = require("axios");
 
 const fs = require("fs");
 
-const cheerio = require('cheerio')
-
+const cheerio = require("cheerio");
 
 data = fs.readFileSync("./sites.csv", "utf-8");
 
+data.split("\r\n").forEach((site) => {
+  axios
+    .get(site)
+    .then((res) => {
+      return res;
+    })
+    .then((data) => {
+      let rawHtml = data.data;
+      const $ = cheerio.load(rawHtml);
+      console.log(site);
 
-console.log(data.split("\r\n")[50]);
-axios
-  .get(data.split("\r\n")[65])
-  .then((res) => {
-    return res;
-  })
-  .then((data) => {
-    let rawHtml = data.data;
-    const $ = cheerio.load(rawHtml)
-    console.log($('a[href^="mailto:"]').text())
-
-
-    
-  });
+      let email = `${$('a[href^="mailto:"]')
+        .text()
+        .trim()
+        .replace(/\s/g, "")}\n`;
+      if (email.length > 2) {
+        fs.appendFile("test", email, (err) => {
+          if (err) {
+            return err;
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      return 0;
+    });
+});
