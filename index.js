cefb7cb5301ac636264acc1ac08ffc54dcb05af8
abdel-boudaoud <abdelkaderfarm@gmail.com>
2025-07-01 21:04:50 +0100

    non profit emails

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
-
 const fs = require("fs");
-
 const cheerio = require("cheerio");
+
+
 let cleanEmail = (email) => {
   let tlds = [".org", ".com", ".net", ".cpa", ".pro"];
 
@@ -13,7 +13,11 @@ let cleanEmail = (email) => {
   }
 };
 
-let data = fs.readFileSync("./Alabama CPA - sites only.csv", "utf-8");
+
+let file = `./non profit  - sites only.csv`;
+let data = fs.readFileSync(file, "utf-8");
+
+
 
 data.split("\r\n").forEach((site) => {
   axios
@@ -33,7 +37,7 @@ data.split("\r\n").forEach((site) => {
         .trim()
         .replace(/\s/g, "")}\n`;
       if (email.length > 2 && email.includes("@")) {
-        fs.appendFile("emails", cleanEmail(email), (err) => {
+        fs.appendFile("emailsAl", cleanEmail(email), (err) => {
           if (err) {
             return err;
           }
@@ -46,46 +50,4 @@ data.split("\r\n").forEach((site) => {
       return 0;
     });
 });
-// const sites = data.split("\r\n");
-// async function getEmails(site) {
-//   try {
-//     const res = await axios.get(site, {
-//       timeout: 60000,
-//     });
 
-//     const $ = cheerio.load(res.data);
-
-//     const email = `${$('a[href^="mailto:"]')
-//       .text()
-//       .trim()
-//       .replace(/\s/g, "")}\n`;
-//     if (email.length > 2 && email.includes("@")) {
-//       fs.appendFile("emails", email, (err) => {
-//         if (err) {
-//           return err;
-//         }
-//       });
-//     } else {
-//       return 0;
-//     }
-//   } catch (err) {
-//     console.log(err);
-
-//   }
-// }
-// async function main (){
-// for (const site of sites) {
-//         if (!site) continue; // Skip any empty lines if filter wasn't perfect
-
-//         // This is the crucial change: await the getEmails function call
-//         // This makes the loop sequential, waiting for each site to finish or timeout
-//         const success = await getEmails(site.trim()); // trim site just in case
-
-//         // You might want a small delay *between* sites even if they succeed/fail quickly
-//         // to be polite to servers. Uncomment and adjust if needed:
-//         // await new Promise(resolve => setTimeout(resolve, 1000)); // 1-second delay
-//     }
-
-// }
-
-// main()
