b32cff0eecebeb872efce01dc3ec3655c0166350
abdel-boudaoud <abdelkaderfarm@gmail.com>
2025-07-07 15:51:59 +0100

    finishing up nodemailer script

--- emails-sender.js
+++ emails-sender.js
@@ -1,12 +1,26 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-let transporter = nodemailer.createTransport ({
-    service: 'gmail', 
-    auth:{
-        user:process.env.GMAIL_USER, 
-        pass:process.env.GMAIL_PASSWORD
-    }
-})
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
 
+let mailOptions = {
+  from: process.env.GMAIL_USER,
+  to: process.env.TARGET,
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
